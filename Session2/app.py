f4d25b68853de355002b137c40f17eefb7694f4e
thanh tuan <anthanhtuan@gmail.com>
2018-03-08 20:42:59 +0700

    Submitted Session 4 homework

--- Session2/app.py
+++ Session2/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
-import mlab
+from flask import Flask, render_template, redirect, url_for, request
+import mlab_connect
 from models.service import Service
 
-mlab.connect()
+mlab_connect.connect()
 
 app = Flask(__name__)
 
@@ -19,6 +19,35 @@ def index():
 def search(gender):
     services = Service.objects(gender=gender, yob__lte = 1998, height__gte = 160, address__contains = "Ha Noi")
     return render_template("search.html", all_services=services)
+
+@app.route('/admin')
+def admin():
+    services = Service.objects()
+    return render_template('admin.html', services = services)
+
+@app.route('/delete/<service_id>')
+def delete(service_id):
+    service_to_delete = Service.objects().with_id(service_id)
+
+    if service_to_delete is None:
+        return "Not found"
+
+    service_to_delete.delete()
+    return redirect(url_for("admin"))  #"Deleted " + service_id #url_for dẫn đến hàm
+
+@app.route('/new-service', methods=["GET", "POST"])
+def create():
+    if request.method == "GET":
+        return render_template('new-service.html')
+    elif request.method == "POST":
+        form = request.form
+        name = form["name"]
+        yob = form["yob"]
+        phone = form["phone"]
+
+        new_service = Service(name=name, yob=yob, phone=phone, gender=1, height=170, address="Hà Nội", status=True)
+        new_service.save()
+        return "{0} {1} {2} posted".format(name, yob, phone)
 
 if __name__ == '__main__':
   app.run(debug=True)
--- Session2/query.py
+++ Session2/query.py
@@ -1,10 +1,25 @@
-import mlab
+import mlab_connect
 from models.service import Service
 
-mlab.connect()
+mlab_connect.connect()
 
-all_services = Service.objects() # lay tat car documents trong collection
+# all_services = Service.objects() # lay tat car documents trong collection
+#
+# first_service = all_services[0]
+#
+# print(first_service.name) # lay key name trong dict
 
-first_service = all_services[0]
+id_to_find = "5a955b13e090f21bac346a71"
 
-print(first_service.name) # lay key name trong dict
+# hera = Service.objects.get(id = id_to_find) # chương trình sẽ dừng
+hera = Service.objects().with_id(id_to_find)
+
+print(hera.to_mongo()) # print tất cả của Doc
+
+if hera is not None:
+    print(hera.name) # print name của Doc
+    hera.update(set__status=False, set__yob=2000) # update gía trị  mới cho Doc
+    hera.reload() #cập nhật lại giá trị cho biến đang lưu trên máy, lấy từ database trên mạng
+    print(hera.to_mongo()) # print tất cả của Doc
+else:
+    print("not found")
